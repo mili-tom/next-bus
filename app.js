2ce385fdd0ee41eaedc2a992d86cc924abd135c7
Milijana Tomic <mlgalic@gmail.com>
2020-05-10 02:55:54 -0500

    Getting street-key data when user clicks on a particular street

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const apiKey = "eM0x2PJKCyqqY7BOlun";
 const search = document.querySelector('form');
+const listElem = document.querySelector('.streets');
+const streetElem = document.querySelector('a');
 
-search.addEventListener('submit', function(event) {
+search.addEventListener('submit', event => {
   const input = event.target.querySelector('input');
   event.preventDefault();
   console.log(input.value); 
@@ -14,16 +16,18 @@ fetch(`https://api.winnipegtransit.com/v2/streets.json?name=${nameOfStreet}&usag
     if (response.ok) {
       return response.json();
     } else {
-      throw new Error("Sorry, error was occured!")
+      throw new Error('Sorry, error was occured!')
     }
   })  
-  .then(json => json.streets.forEach(street => {
-    insertStreetList(street);
-  }))
+  .then(json => 
+    json.streets.forEach(street => {
+    insertStreetsList(street);
+    })
+  )
+  listElem.innerHTML = '';  
 } 
 
-function insertStreetList(street) {
-  const listElem = document.querySelector('.streets');    
+function insertStreetsList(street) { 
   listElem.insertAdjacentHTML('afterbegin',
     `<a href="#" data-street-key=${street.key}>${street.name}</a>`
   );  
@@ -37,3 +41,10 @@ function insertStreetList(street) {
 //   .then(response => response.json())
 //   .then(json => console.log(json))
 
+listElem.addEventListener('click', function(event) {
+  if (event.target.nodeName === 'A') {
+    const streetKey = event.target.dataset.streetKey;
+    console.log(streetKey);
+    findStops(streetKey);
+  }
+})
