b858219dc2f7ef11e2d8ed93b644107b0f858d26
Milijana Tomic <mlgalic@gmail.com>
2020-05-10 01:31:07 -0500

    Listing streets in DOM based on user entry

--- app.js
+++ app.js
@@ -10,8 +10,30 @@ search.addEventListener('submit', function(event) {
 
 function findStreet(nameOfStreet) {
 fetch(`https://api.winnipegtransit.com/v2/streets.json?name=${nameOfStreet}&usage=short&api-key=${apiKey}`)
-  .then(response => response.json())
-  .then(json => console.log(json))
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("Sorry, error was occured!")
+    }
+  })  
+  .then(json => json.streets.forEach(street => {
+    insertStreetList(street);
+  }))
 } 
+
+function insertStreetList(street) {
+  const listElem = document.querySelector('.streets');    
+  listElem.insertAdjacentHTML('afterbegin',
+    `<a href="#" data-street-key=${street.key}>${street.name}</a>`
+  );  
+}
   
+// fetch(`https://api.winnipegtransit.com/v2/stops/10064.json?usage=short&api-key=eM0x2PJKCyqqY7BOlun`)
+//   .then(response => response.json())
+//   .then(json => console.log(json))
+
+//fetch(`https://api.winnipegtransit.com/v2/stops/10064.schedule.json?api-key=eM0x2PJKCyqqY7BOlun`)
+//   .then(response => response.json())
+//   .then(json => console.log(json))
 
