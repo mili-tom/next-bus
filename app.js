cedd20d615e28c5526ce5d9fc929a69bdc7b6375
Milijana Tomic <mlgalic@gmail.com>
2020-05-11 22:34:48 -0500

    Extracting obtained data into 2 parts: route-schedules and stop-schedules

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const apiKey = "eM0x2PJKCyqqY7BOlun";
 const search = document.querySelector('form');
 const listElem = document.querySelector('.streets');
+const table = document.querySelector('tbody');
 
 search.addEventListener('submit', event => {
+  table.innerHTML = '';
   const input = event.target.querySelector('input');
   event.preventDefault();
-  findStreet(input.value);
+  findStreet(input.value);  
 })
 
 function findStreet(nameOfStreet) {
@@ -14,7 +16,7 @@ fetch(`https://api.winnipegtransit.com/v3/streets.json?name=${nameOfStreet}&usag
     if (response.ok) {
       return response.json();
     } else {
-      throw new Error('Sorry, error was occured!')
+      throw new Error('Sorry, error was occured!');
     }
   })  
   .then(function(json) {
@@ -44,7 +46,7 @@ function insertEmptyList() {
 listElem.addEventListener('click', function(event) {
   if (event.target.nodeName === 'A') {    
     const streetKey = parseInt(event.target.dataset.streetKey);
-    console.log(streetKey);
+    //console.log(streetKey);
     findStops(streetKey);
 
     const selectedStreet = event.target.textContent;
@@ -54,12 +56,32 @@ listElem.addEventListener('click', function(event) {
 
 function findStops(key) {
   fetch(`https://api.winnipegtransit.com/v3/stops.json?street=${key}&usage=long&api-key=${apiKey}`)
-    .then(response => response.json())
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Sorry, error was occured!');
+      }
+    })
     .then(json => {
+      //console.log(json);
       json.stops.forEach(stop => {
         fetch(`https://api.winnipegtransit.com/v3/stops/${stop.key}/schedule.json?api-key=${apiKey}`)
-          .then(response => response.json())
-          .then(json => console.log(json))
+          .then(response => {
+            if (response.ok) {
+              return response.json();
+            } else {
+              throw new Error('Sorry, error was occured!')
+            }
+          })
+          .then(json => {
+            const stopSchedule = json['stop-schedule']['stop'];
+            const routes = json['stop-schedule']['route-schedules'];         
+            routes.forEach(route => {
+              console.log(route);
+              //displayBus(route, stopSchedule);
+            })
+          })
       });
     });
 }
@@ -69,6 +91,3 @@ function displayStreet(street) {
   titleElem.textContent = `Displaying results for ${street}`;
 }
 
-//fetch(`https://api.winnipegtransit.com/v3/stops/10064/schedule.json?api-key=eM0x2PJKCyqqY7BOlun`)
-//   .then(response => response.json())
-//   .then(json => console.log(json))
